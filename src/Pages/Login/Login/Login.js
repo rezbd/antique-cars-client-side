d14f14f9e65b64a319627b292e6259b932da3c53
rezwan <rezwan6@gmail.com>
2021-11-12 11:07:34 +0600

    login and register updated with functionality

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        console.log(field, value);
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+    const handleLoginSubmit = e => {
+        alert('hello')
+        e.preventDefault();
+    }
     return (
         <div className="row px-2">
             <div className="col-12 col-md-4 mx-auto">
                 <h2 className="my-5">Please Login</h2>
-                <Form>
+                <Form onSubmit={handleLoginSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control onChange={handleOnChange} name="email" type="email" placeholder="Enter email" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={handleOnChange} name="password" type="password" placeholder="Password" />
                     </Form.Group>
                     <Button className="w-100 d-block my-4" variant="primary" type="submit">
                         Submit
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const Register = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+    const handleLoginSubmit = e => {
+        if (loginData.password !== loginData.password2) {
+            alert('Your password did not match');
+            return
+        }
+        e.preventDefault();
+    }
+
     return (
         <div className="row px-2">
             <div className="col-12 col-md-4 mx-auto">
                 <h2 className="my-5">Register</h2>
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Your Name</Form.Label>
-                        <Form.Control type="email" placeholder="Enter your name" />
-                    </Form.Group>
+                <Form onSubmit={handleLoginSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onChange={handleOnChange} name="email" type="email" placeholder="Enter email" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={handleOnChange} name="password" type="password" placeholder="Password" />
                     </Form.Group>
-                    <Form.Group className="mb-3">
+                    <Form.Group className="mb-3" controlId="formBasicPassword2">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" />
+                        <Form.Control onChange={handleOnChange} name="password2" type="password" placeholder="Confirm Password" />
                     </Form.Group>
                     <Button className="w-100 d-block my-4" variant="primary" type="submit">
                         Submit
                     </Button>
                     <NavLink
                         to="/login">
-                        <Button style={{ textDecoration: 'none' }} variant="link">Have an account? Please Login</Button>
+                        <Button style={{ textDecoration: 'none' }} variant="link">New User? Please Login</Button>
                     </NavLink>
                 </Form>
             </div>
