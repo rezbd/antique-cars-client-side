74375d4e8c06ab7f3364148079ee86bab4e61b04
rezwan <rezwan6@gmail.com>
2021-11-15 20:39:13 +0600

    going to implement delete on my orders

--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -4,43 +4,32 @@ import useAuth from "../../hooks/useAuth";
 const MyOrders = () => {
 
     const { user } = useAuth();
-    // console.log(user);
-    // const email = user.email;
+
+    const [services, setServices] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:5000/myOrders/${user?.email}`)
             .then((res) => res.json())
-            .then((data) => console.log(data));
+            .then((data) => setServices(data));
     }, [user]);
 
     return (
         <div>
             <h2>My Orders</h2>
-            {/* <div className="services">
+            <div className="services">
                 <div className="row container">
                     {services?.map((pd) => (
-                        <div className="col-md-4">
-                            <div className="service border border p-3">
-                                <div className="services-img ">
-                                    <img className="w-100" src={pd?.image} alt="" />
-                                </div>
-
-                                <h6>{pd?.name}</h6>
-                                <h4>{pd?.model}</h4>
-                                <p>{pd?.description}</p>
-                                <h3 className="text-danger"> Cost :{pd?.price}$</h3>
-
-                                <button
-                                    onClick={() => handleDelete(pd?._id)}
-                                    className="btn btn-danger"
-                                >
-                                    Cancel
-                                </button>
+                        <div key={pd._id} className="col-12 col-md-4">
+                            <div className="border border-3 p-3">
+                                <h6>My Name: {pd?.name}</h6>
+                                <h4>Selected Car: {pd?.carName}</h4>
+                                <p>Shipping Address: {pd?.address}</p>
+                                <button className="btn btn-danger">Cancel Order</button>
                             </div>
                         </div>
                     ))}
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 };
