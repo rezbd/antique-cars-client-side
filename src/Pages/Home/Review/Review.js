07a0e1db64a72a95bdda03e72a26c69b10ffca08
rezwan <rezwan6@gmail.com>
2021-11-20 18:30:24 +0600

    implemented dynamic user ratings to display

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from 'react-rating';
 import './Review.css';
 
 const Review = ({ feedback }) => {
@@ -7,8 +8,15 @@ const Review = ({ feedback }) => {
         <div className="review-each">
             <h4>{name}</h4>
             <h6 className="my-3">{occupation}</h6>
-            <p className="my-3">{review}</p>
-            <h6 className="my-3">Rating: {rating}/5</h6>
+            <p className="my-2">{review}</p>
+            {/* <h6 className="my-3">Rating: {rating}/5</h6> */}
+            <Rating
+                readonly
+                initialRating={rating}
+                emptySymbol="far fa-star star-color"
+                fullSymbol="fas fa-star star-color"
+                className="my-3"
+            ></Rating>
         </div>
     );
 };
